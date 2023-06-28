f728568be906b729a957a06dd25a5d9860507ae3
alexleems19 <uberman06@gmail.com>
2023-06-27 20:50:22 -0400

    Test Application completed

--- src/main/java/org/example/StreamingJob.java
+++ src/main/java/org/example/StreamingJob.java
@@ -19,7 +19,7 @@
 package org.example;
 
 
-import
+import org.apache.flink.connector.kafka.sink.KafkaSinkBuilder;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
@@ -31,6 +31,10 @@ import org.apache.flink.connector.kafka.sink.KafkaSink;
 import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Properties;
 /**
  * Skeleton for a Flink Streaming Job.
@@ -46,64 +50,36 @@ import java.util.Properties;
  */
 public class StreamingJob {
 
-	final static String inputTopic = "input-topic";
-	final static String outputTopic = "output-topic";
-	final static String jobTitle = "WordCount";
+		private static final String FILE_PATH = "src/main/resources/application.properties";
 
-	public static void main(String[] args) throws Exception {
-		final String bootstrapServers = args.length > 0 ? args[0] : "localhost:9092";
-		// set up the streaming execution environment
-		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		public static void main(String[] args) {
+			try {
+				//Load properties from config file
+				Properties properties = new Properties();
+				properties.load(new FileReader(FILE_PATH));
+				StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment().setParallelism(1);
 
-		Properties properties = new Properties();
-
-		properties.setProperty("bootstrapServer", "HostName");
-		properties.setProperty("")
-
+				String Topic = properties.getProperty("EventHubName");
+				String broker = properties.getProperty("bootstrapServers");
+				String consumerGroup = properties.getProperty("consumerGroupID");
 
 
-		if(url != null)
+				KafkaSource<String> source = KafkaSource.<String>builder()
+						.setBootstrapServers(broker)
+						.setTopics(Topic)
+						.setGroupId(consumerGroup)
+						.setStartingOffsets(OffsetsInitializer.earliest())
+						.setValueOnlyDeserializer(new SimpleStringSchema())
+						.build();
 
-		KafkaSource<String> source = KafkaSource.<String>builder()
-				.setBootstrapServers(bootstrapServers)
-				.setTopics(inputTopic)
-				.setGroupId("my-group")
-				.setStartingOffsets(OffsetsInitializer.earliest())
-				.setValueOnlyDeserializer(new SimpleStringSchema())
-				.build();
+				DataStream<String> kafka = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
+				kafka.print();
 
-		KafkaRecordSerializationSchema<String> serializer = KafkaRecordSerializationSchema.builder()
-				.setValueSerializationSchema(new SimpleStringSchema())
-				.setTopic(outputTopic)
-				.build();
-
-		KafkaSink<String> sink = KafkaSink.<String>builder()
-				.setBootstrapServers(bootstrapServers)
-				.setRecordSerializer(serializer)
-				.build();
-
-		DataStream<String> text = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
-		/*
-		 * Here, you can start creating your execution plan for Flink.
-		 *
-		 * Start with getting some data from the environment, like
-		 * 	env.readTextFile(textPath);
-		 *
-		 * then, transform the resulting DataStream<String> using operations
-		 * like
-		 * 	.filter()
-		 * 	.flatMap()
-		 * 	.join()
-		 * 	.coGroup()
-		 *
-		 * and many more.
-		 * Have a look at the programming guide for the Java API:
-		 *
-		 * http://flink.apache.org/docs/latest/apis/streaming/index.html
-		 *
-		 */
-
-		// execute program
-		env.execute("Flink Streaming Java API Skeleton");
+				env.execute("Testing flink consumer");
+			} catch(FileNotFoundException e){
+				System.out.println("FileNoteFoundException: " + e);
+			} catch (Exception e){
+				System.out.println("Failed with exception " + e);
+			}
 	}
 }
