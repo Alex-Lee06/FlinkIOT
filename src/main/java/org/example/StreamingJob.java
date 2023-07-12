7918e91638b518ec6ec944be96ed32e31b3daa92
alexleems19 <uberman06@gmail.com>
2023-07-12 11:16:37 -0400

    Changed port number, added security protocol(SSL), added sasl mechanism(PLAIN), and sasl.jaas.config.

--- src/main/java/org/example/StreamingJob.java
+++ src/main/java/org/example/StreamingJob.java
@@ -19,23 +19,25 @@
 package org.example;
 
 
-import org.apache.flink.connector.kafka.sink.KafkaSinkBuilder;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.util.Collector;
+import org.apache.flink.api.common.serialization.SimpleStringEncoder;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.configuration.MemorySize;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
-import org.apache.flink.connector.kafka.sink.KafkaSink;
-import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
-import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.connector.file.sink.FileSink;
+import org.apache.flink.core.fs.Path;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
+import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy;
+import org.apache.kafka.clients.consumer.OffsetResetStrategy;
+import org.apache.kafka.common.TopicPartition;
 
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.Properties;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.HashSet;
+
 /**
  * Skeleton for a Flink Streaming Job.
  *
@@ -52,7 +54,7 @@ public class StreamingJob {
 
 		private static final String FILE_PATH = "src/main/resources/application.properties";
 
-		public static void main(String[] args) {
+		public static void main(String[] args){
 			try {
 				//Load properties from config file
 				//Properties properties = new Properties();
@@ -63,23 +65,49 @@ public class StreamingJob {
 				//String broker = properties.getProperty("bootstrapServers");
 				//String consumerGroup = properties.getProperty("consumerGroupID");
 
+				final HashSet<TopicPartition> partitionSet = new HashSet<TopicPartition>(Arrays.asList(
+						new TopicPartition("iothub-ehub-telemetryd-25096833-a002d81f0c", 0),
+						new TopicPartition("iothub-ehub-telemetryd-25096833-a002d81f0c", 1)
+				));
+
 				String password = "Endpoint=sb://ihsuprodblres094dednamespace.servicebus.windows.net/;SharedAccessKeyName=iothubowner;SharedAccessKey=tOKQsqHOlyuv1xDZKCgTkxNjcoiLX58Xcadp85jri+k=;EntityPath=iothub-ehub-telemetryd-25096833-a002d81f0c";
 				String connectionString= "HostName=telemetrydocker.azure-devices.net;DeviceId=myDevice;SharedAccessKey=e7DCUMzA1iESOyEnfwgjZOWSVUTNnEmHxU/KyQymVvI=";
 				KafkaSource<String> source = KafkaSource.<String>builder()
-						.setBootstrapServers("ihsuprodblres094dednamespace.servicebus.windows.net:443")
+						//.setBootstrapServers("137.135.102.226:9093")
+						.setBootstrapServers("ihsuprodblres094dednamespace.servicebus.windows.net:9093")
 						.setTopics("iothub-ehub-telemetryd-25096833-a002d81f0c")
-						.setGroupId("iotflinkmessages")
-						.setProperty("security.protocol", "SSL")
+						//.setPartitions(partitionSet)
+						.setGroupId("$Default")
+						.setProperty("partition.discovery.interval.ms", "10000")
+						.setClientIdPrefix("iotflinkmessages")
+						//.setProperty("security.protocol", "SASL_SSL")
+						//.setProperty("sasl.mechanism", "SCRAM-SHA-256")
+						.setProperty("security.protocol", "SASL_PLAINTEXT")
 						.setProperty("sasl.mechanism", "PLAIN")
-						.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"" + connectionString + "\" password=\"" + password + "\";")
-						.setStartingOffsets(OffsetsInitializer.earliest())
+						//.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"" + connectionString + "\" password=\"" + password + "\";")
+						.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"HostName=telemetrydocker.azure-devices.net;DeviceId=myDevice;SharedAccessKey=e7DCUMzA1iESOyEnfwgjZOWSVUTNnEmHxU/KyQymVvI=\" password=\"Endpoint=sb://flinkev.servicebus.windows.net/;SharedAccessKeyName=iothubroutes_telemetrydocker;SharedAccessKey=NSAjLJPAl9gyDIB8LsKYfyCs36G1bXZDn+AEhPCsTdw=;EntityPath=iotevflink\";")
+						.setStartingOffsets(OffsetsInitializer.committedOffsets(OffsetResetStrategy.EARLIEST))
 						.setValueOnlyDeserializer(new SimpleStringSchema())
 						.build();
 
 				DataStream<String> kafka = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
 				kafka.print();
 
-				env.execute("Testing flink consumer");
+				String outputPath  = "abfs://iotmessages@gen2alleestorage.dfs.core.windows.net/messagesfromiot/";
+
+				final FileSink<String> sink = FileSink
+						.forRowFormat(new Path(outputPath), new SimpleStringEncoder<String>("UTF-8"))
+						.withRollingPolicy(
+								DefaultRollingPolicy.builder()
+										.withRolloverInterval(Duration.ofMinutes(2))
+										.withInactivityInterval(Duration.ofMinutes(3))
+										.withMaxPartSize(MemorySize.ofMebiBytes(5))
+										.build())
+						.build();
+
+				kafka.sinkTo(sink);
+
+				env.execute("IoT-Flink-Gen2 sink");
 			} catch(FileNotFoundException e){
 				System.out.println("FileNoteFoundException: " + e);
 			} catch (Exception e){
